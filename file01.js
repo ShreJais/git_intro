8bb7fef9f450d47af2614d4d28bdb2488f5a4445
shreyas <shrejais1@gmail.com>
2022-08-19 12:58:48 +0530

    sheldon greeter

--- file01.js
+++ file01.js
@@ -1,4 +1,7 @@
 function greet(name) {
+    console.log('Knock knock knock' + name)
+    console.log('Knock knock knock' + name)
+    console.log('Knock knock knock' + name)
     console.log('hello' + name)
 }
 

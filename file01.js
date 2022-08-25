9870449de7d3b146dd112158ea85e9b4c2802d1d
ShreJais <shrejais1@gmail.com>
2022-08-25 12:33:12 +0530

    add new line 'Nice to have you here.'
    Signed-off-by: ShreJais <shrejais1@gmail.com>

--- file01.js
+++ file01.js
@@ -3,6 +3,7 @@ function greet(name) {
     console.log('Knock knock knock' + name)
     console.log('Knock knock knock' + name)
     console.log('hello' + name)
+    console.log('Nice to have you here.')
 }
 
 greet('shreyas')

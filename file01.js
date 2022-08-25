84ca1be515391b1268de021798486bfaaee52b01
ShreJais <shrejais1@gmail.com>
2022-08-25 12:48:29 +0530

    Add other line in file01.js
    Signed-off-by: ShreJais <shrejais1@gmail.com>

--- file01.js
+++ file01.js
@@ -4,6 +4,7 @@ function greet(name) {
     console.log('Knock knock knock' + name)
     console.log('hello' + name)
     console.log('Nice to have you here.')
+    console.log('Would you like some tea?')
 }
 
 greet('shreyas')
